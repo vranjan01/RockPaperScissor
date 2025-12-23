ab17eead3708b7c00b2733d2aaeea9c5b3e0a81d
Vaishnavi Ranjan <ranjanvaishnavi0107@gmail.com>
2025-12-23 17:27:03 +0530

    Logic in js added

---
+++ script.js
@@ -0,0 +1,17 @@
+//let userChoice = String
+let computerChoice = getRndInteger(1,3)
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+let choice= ["rock","scissors","paper"]
+let computerChoiceStr = choice[computerChoice]
+
+if(userChoice === computerChoiceStr)
+{
+    return "It is a Tie!"
+}
+else if( (userChoice == "rock" && computerChoiceStr == "scissor") || (userChoice == "paper" && computerChoiceStr == "rock") || (userChoice == "scissor" && computerChoiceStr == "paper")){
+    return "You Win!"
+}
+else
+    return "Computer Wins!"
